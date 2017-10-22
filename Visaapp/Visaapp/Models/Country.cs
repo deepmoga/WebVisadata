e4f6ffca696a1bf06476dc503515fd6e8e439d29
deepmoga <gagansngh966@gmail.com>
2017-10-22 01:47:38 -0700

    create country class

---
+++ Visaapp/Visaapp/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Visaapp.Models
+{
+    public class Country
+    {
+        public int id { get; set; }
+        [Display(Name="Country Name")]
+        [Required (ErrorMessage="Please Enter Country Name")]
+        public string CountryName { get; set; }
+    }
+}
